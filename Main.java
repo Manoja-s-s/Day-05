d715068aaf439b64cc0cb463498ea8ee482e8f7f
Manoja S S <manojass.in@gmail.com>
2025-11-21 23:53:44 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main { // Creating a Variable using var to store age
+
+	public static void main(String[] args) {
+		var age = 23;
+		System.out.println(age);
+	}
+
+}
---
+++ Main2.java
@@ -0,0 +1,8 @@
+class Main2 {
+
+	public static void main(String[] args) {
+		var firstName = "Manoja";
+		System.out.println(firstName);
+	}
+
+}
